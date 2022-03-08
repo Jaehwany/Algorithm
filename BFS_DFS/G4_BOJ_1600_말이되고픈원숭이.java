4f22fb13f02200fe62a4bbffe3914aa149190ee5
LeeJaeHwan <97875998+Jaehwany@users.noreply.github.com>
2022-03-09 02:53:52 +0900

    Update and rename BOJ_1600_말이되고픈원숭이.java to G4_BOJ_1600_말이되고픈원숭이.java

--- BFS_DFS/G4_BOJ_1600_말이되고픈원숭이.java
+++ BFS_DFS/G4_BOJ_1600_말이되고픈원숭이.java
@@ -1,4 +1,3 @@
-package bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,19 +5,23 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class BOJ_1600_말이되고픈원숭이 {
+public class G4_BOJ_1600_말이되고픈원숭이 {
+	//변수 선언
 	static int K,M,N;
 	static int map[][];
 	static boolean visit[][][];
+	
+	//4방 탐색 선언
 	static int dx[]= {1,-1,0,0};
 	static int dy[]= {0,0,1,-1};
+	
+	//말 8방 탐색 선언
 	static int dx2[]= {1,2,2,1,-1,-2,-2,-1};
 	static int dy2[]= {-2,-1,1,2,2,1,-1,-2};
 	
-	
+	//좌표 클래스 선언
 	static class CO{
 		int x,y,k,move;
-
 		public CO(int x, int y, int k, int move) {
 			super();
 			this.x = x;
@@ -46,7 +49,7 @@ public class BOJ_1600_말이되고픈원숭이 {
 		}
 		bfs(new CO(0,0,K,0));
 	}
-	
+	//BFS
 	static void bfs(CO start){
 		Queue<CO> queue = new LinkedList<>();
 		queue.offer(start);
@@ -54,24 +57,21 @@ public class BOJ_1600_말이되고픈원숭이 {
 		
 		while(!queue.isEmpty()) {
 			CO cur = queue.poll();
-			
 			if(cur.x==M-1&&cur.y==N-1) {
 				System.out.println(cur.move);
 				return;
 			}
-			
+			//인접 4방향 탐색
 			for(int d=0;d<4;d++) {
 				int nx=cur.x+dx[d];
-				int ny=cur.y+dy[d];
-				
-				if(nx<0||ny<0||nx>=M||ny>=N) continue;
-				
+				int ny=cur.y+dy[d];	
+				if(nx<0||ny<0||nx>=M||ny>=N) continue;	
 				if(!visit[cur.k][ny][nx]&&map[ny][nx]==0) {
 					queue.offer(new CO(nx,ny,cur.k,cur.move+1));
 					visit[cur.k][ny][nx]=true;
 				}
 			}
-			
+			//점프 횟수가 남은 경우 말 8방 탐색
 			if(cur.k>0) {
 				for(int d=0;d<8;d++) {
 					int nx=cur.x+dx2[d];
